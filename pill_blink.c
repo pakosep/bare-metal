f827bfc22346db29cae06a4d6580cfe6c1fb8b79
Sepako <sepako@o2.pl>
2021-05-15 07:49:57 +0200

    ok

--- pill_blink.c
+++ pill_blink.c
@@ -9,7 +9,7 @@ void __attribute__ ((weak, naked)) reset_handler(void) {
         for (int i = 0; i < 1000000; ++i) __asm__("nop");
 
         (*(volatile unsigned short *)(0x40011014)) = (1 << 13);
-        for (int i = 0; i < 500000; ++i) __asm__("nop");
+        for (int i = 0; i < 500001; ++i) __asm__("nop");
     }
 }
 
